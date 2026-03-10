f54d9a050f6b2d614b57a95a6c4c1b21dd981ffd
Felix Hohn <felix.hohn@lavita.de>
2026-03-10 13:42:37 +0100

    add action to deploy to github actions

--- dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"f4Y652NU3PKkpgQK2xNsYEZTm88X5xhldEyoQSeuSOI=\"\n}"
--- dev/static/chunks/app_tests_[slug]_page_tsx_28a55fef._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b00b1283._.js"
-],
-    source: "dynamic"
-});
